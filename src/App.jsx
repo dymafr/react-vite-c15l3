950ddb71a1fae0b14b252d976d2508cec7c00731
dymafr <contact@dyma.fr>
2023-11-03 09:51:39 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
+import styles from './App.module.scss';
+import { seedRecipes } from './data/seed';
+
+seedRecipes();
+
+function App() {
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
